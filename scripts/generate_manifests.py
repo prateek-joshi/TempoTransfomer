8b7d01305b3f4c268f80b4e1b37340580d2f45c5
Prateek Joshi <pjoshi22900@gmail.com>
2022-04-10 15:56:33 +0530

    Update script and added manifest file

--- scripts/generate_manifests.py
+++ scripts/generate_manifests.py
@@ -1,5 +1,4 @@
 import xml.etree.ElementTree as ET
-from p_tqdm import p_umap
 import pandas as pd
 import argparse
 import librosa
@@ -28,33 +27,51 @@ def duration(audiopath):
     return math.floor(librosa.get_duration(y=y, sr=sr))
 
 def get_details_gtzan(gtzan_stats_path):
-    gtzan_df = pd.read_csv(gtzan_stats_path,usecols=['filename','tempo mean'], nrows=5)
+    gtzan_df = pd.read_csv(gtzan_stats_path,usecols=['filename','tempo mean'])
     gtzan_df.columns = ['audio_filepath', 'tempo']
     gtzan_df['genre'] = gtzan_df.loc[:,'audio_filepath'].str.split('.').str[0]
     gtzan_df['audio_filepath'] = os.path.join('E:\\','TempoTransformer','data','GTZAN','songs')+'\\'+gtzan_df['genre']+'\\'+gtzan_df['audio_filepath']
     gtzan_df['duration'] = gtzan_df.loc[:,'audio_filepath'].apply(duration)
     logging.info(f'Found {len(gtzan_df)} audio files in {gtzan_stats_path}')
+    # logging.info('Written GTZAN manifest')
     return gtzan_df
 
-def get_details_ebroom(song_node):
+def get_details_ebroom(root):
     songs_folder = os.path.join('E:\\','TempoTransformer','data','ExtendedBallroom','songs')
-    genre_folder = os.path.join(songs_folder, genre_node.tag)
-    datalist = []
-    
-    tempo = int(song_node.get('bpm'))
-    filename = os.path.join(genre_folder,song_node.get('id')+'.wav')
-    if not os.path.exists(filename):
-        return
-    audio_duration = duration(filename)
-    datalist.append([filename, tempo, genre_node.tag, audio_duration])
-    ebroom_df = pd.DataFrame(datalist, columns=['audio_filepath','tempo','genre','duration'])
-    logging.info(f'Found {len(ebroom_df)} audio files in {ebroom_stats_path}')
+    ebroom_df = pd.DataFrame(columns=['audio_filepath','tempo','genre','duration'])
+    for genre_node in root:
+        genre_folder = os.path.join(songs_folder, genre_node.tag)
+        for song_node in genre_node:
+            song_path = os.path.join(genre_folder, song_node.get('id') + '.wav')
+            if not os.path.exists(song_path):
+                logging.exception(f'Could not find file {song_path}')
+                continue
+            try:
+                song_duration = duration(song_path)
+                bpm = int(song_node.get('bpm'))
+                genre = genre_node.tag
+                ebroom_df.loc[len(ebroom_df.index)] = [song_path, bpm, genre, song_duration]
+            except:
+                logging.exception(f'Could not extract details of {song_path}')
+        logging.info(f'Finished {genre_node.tag}')
+    logging.info(f'Found {len(ebroom_df)} audio files in {songs_folder}')
     return ebroom_df
+    # logging.info('Written ExtendedBallroom manifest')
+
+    
+    # tempo = int(song_node.get('bpm'))
+    # filename = os.path.join(genre_folder,song_node.get('id')+'.wav')
+    # if not os.path.exists(filename):
+    #     return
+    # audio_duration = duration(filename)
+    # datalist.append([filename, tempo, genre_node.tag, audio_duration])
+    # ebroom_df = pd.DataFrame(datalist, columns=['audio_filepath','tempo','genre','duration'])
+    # logging.info(f'Found {len(ebroom_df)} audio files in {ebroom_stats_path}')
+    # return ebroom_df
 
 
 
 if __name__=='__main__':
-    ebroom_songs_folder = os.path.join('E:\\','TempoTransformer', 'data', 'ExtendedBallroom', 'songs')
     ANNOT_PATH = {
         'gtzan': 'E:\\TempoTransformer\\data\\GTZAN\\GTZAN-Rhythm_v2_ismir2015_lbd\\stats.csv',
         'ebroom': 'E:\\TempoTransformer\\data\\ExtendedBallroom\\songs\\extendedballroom_v1.1.xml'
@@ -64,9 +81,9 @@ if __name__=='__main__':
     logging.info('Retrieved GTZAN data')
 
     root = open_xml(ANNOT_PATH['ebroom'])
-    res = p_umap(get_details_ebroom,root,num_cpus=4,desc=)
+    ebroom_df = get_details_ebroom(root)
     logging.info('Retrieved ExtendedBallroom data')
-    final_df = pd.concat(gtzan_df, ebroom_df)
+    final_df = pd.concat([gtzan_df, ebroom_df])
     print(final_df.head())
     print('\n')
     print(final_df.tail())
