f616756eaaef78f8b1e3292e5d04ac4d31bdf94c
Prateek Joshi <pjoshi22900@gmail.com>
2022-04-16 17:51:49 +0530

    Added spectrogram generation

--- Temformer/data.py
+++ Temformer/data.py
@@ -22,7 +22,7 @@ class Dataset:
         self.batch_size = batch_size
 
     def get_data(self):
-        data = pd.read_pickle(self.manifest_path, compression=None)
+        data = pd.read_csv(self.manifest_path)
         filenames = data['audio_filepath']
         self.train_filenames, self.val_filenames = train_test_split(filenames,test_size=self.split_ratio,shuffle=False)
         if self.shuffle:
@@ -34,7 +34,7 @@ class Dataset:
         print(f'Found {len(self.train_filenames)} train samples\nFound {len(self.val_filenames)} validation samples')
 
     def get_waveform_and_label(self, filename):
-        df = pd.read_pickle(self.manifest_path, compression=None)
+        df = pd.read_csv(self.manifest_path)
         # print(df.iloc[0,:])
         filename = filename.numpy().decode().replace(r"[b']",'')
         tempo = df.loc[df['audio_filepath']==filename,'tempo'].to_numpy()[0]
@@ -45,27 +45,62 @@ class Dataset:
         return audio, tempo
 
     def get_waveform_generator(self):
-        self.get_data()
         train_waveform_ds = tf.data.Dataset.from_tensor_slices(self.train_filenames).map(lambda filename: tf.py_function(self.get_waveform_and_label, [filename], [tf.float32,tf.float32]))
         val_waveform_ds = tf.data.Dataset.from_tensor_slices(self.val_filenames).map(lambda filename: tf.py_function(self.get_waveform_and_label, [filename], [tf.float32,tf.float32]))
         return train_waveform_ds, val_waveform_ds
 
+    def get_spectrogram_and_label(self, waveform, tempo):
+        # Cast the waveform tensors' dtype to float32.
+        waveform = tf.cast(waveform, dtype=tf.float32)
+        # Convert the waveform to a spectrogram via a STFT.
+        spectrogram = tf.signal.stft(
+            waveform, frame_length=255, frame_step=128)
+        # Obtain the magnitude of the STFT.
+        spectrogram = tf.abs(spectrogram)
+        # Add a `channels` dimension, so that the spectrogram can be used
+        # as image-like input data with convolution layers (which expect
+        # shape (`batch_size`, `height`, `width`, `channels`).
+        spectrogram = spectrogram[..., tf.newaxis]
+        return spectrogram, tempo
+
+    def get_spectrogram_generator(self):
+        self.get_data()
+        wav_train, wav_val = self.get_waveform_generator()
+        spec_train = wav_train.map(self.get_spectrogram_and_label)
+        spec_val = wav_val.map(self.get_spectrogram_and_label)
+        return spec_train, spec_val
+
+    @staticmethod
+    def plot_spectrogram(spectrogram, ax):
+        if len(spectrogram.shape) > 2:
+            assert len(spectrogram.shape) == 3
+            spectrogram = np.squeeze(spectrogram, axis=-1)
+        # Convert the frequencies to log scale and transpose, so that the time is
+        # represented on the x-axis (columns).
+        # Add an epsilon to avoid taking a log of zero.
+        log_spec = np.log(spectrogram.T + np.finfo(float).eps)
+        height = log_spec.shape[0]
+        width = log_spec.shape[1]
+        X = np.linspace(0, np.size(spectrogram), num=width, dtype=int)
+        Y = range(height)
+        ax.pcolormesh(X, Y, log_spec)
+
 if __name__=='__main__':
-    MANIFEST_PATH = pathlib.Path('E:\\TempoTransformer\\data\\final_manifest.pkl')
+    MANIFEST_PATH = 'E:\\TempoTransformer\\data\\final_manifest_new.csv'
     d = Dataset(MANIFEST_PATH, val_split=0.05)
-    train,val = d.get_waveform_generator()
+    train,val = d.get_spectrogram_generator()
 
     rows = 3
     cols = 3
-    n = rows * cols
-    fig, axes = plt.subplots(rows, cols, figsize=(10, 12))
-    for i, (audio, label) in enumerate(train.take(n)):
+    n = rows*cols
+    fig, axes = plt.subplots(rows, cols, figsize=(10, 10))
+
+    for i, (spectrogram, tempo) in enumerate(train.take(n)):
         r = i // cols
         c = i % cols
         ax = axes[r][c]
-        ax.plot(audio.numpy())
-        ax.set_yticks(np.arange(-1.2, 1.2, 0.2))
-        label = label.numpy()
-        ax.set_title(label)
+        Dataset.plot_spectrogram(spectrogram.numpy(), ax)
+        ax.set_title(tempo.numpy())
+        ax.axis('off')
 
     plt.show()
