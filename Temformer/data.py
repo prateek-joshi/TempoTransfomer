79e22a3a21890a845ac72390f96b6c6f2ba98818
Prateek Joshi <pjoshi22900@gmail.com>
2022-04-17 17:28:06 +0530

    Add option between CQT and FFT

--- Temformer/data.py
+++ Temformer/data.py
@@ -9,17 +9,28 @@ import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 
 # Set the seed value for experiment reproducibility.
-seed = 42
-tf.random.set_seed(seed)
-np.random.seed(seed)
+SEED = 42
+tf.random.set_seed(SEED)
+np.random.seed(SEED)
 
 
-class Dataset:
-    def __init__(self, manifest_path, shuffle=True, val_split=0.1, batch_size=32) -> None:
+class TempoDataset:
+    def __init__(
+        self, manifest_path, 
+        shuffle=True, 
+        val_split=0.1, 
+        batch_size=32,
+        sample_rate=22050,
+        n_fft=512,
+        fft = True
+    ):
         self.manifest_path = pathlib.Path(manifest_path)
         self.shuffle = shuffle
         self.split_ratio = val_split
         self.batch_size = batch_size
+        self.sample_rate = sample_rate
+        self.n_fft = n_fft
+        self.fft = fft
 
     def get_data(self):
         data = pd.read_csv(self.manifest_path)
@@ -27,8 +38,8 @@ class Dataset:
         self.train_filenames, self.val_filenames = train_test_split(filenames,test_size=self.split_ratio,shuffle=False)
         if self.shuffle:
             # Mention seed explicitly to preserve order
-            self.train_filenames = tf.random.shuffle(self.train_filenames, seed=seed)
-            self.val_filenames = tf.random.shuffle(self.val_filenames, seed=seed)
+            self.train_filenames = tf.random.shuffle(self.train_filenames, seed=SEED)
+            self.val_filenames = tf.random.shuffle(self.val_filenames, seed=SEED)
             
         # print(self.train_filenames[0], self.train_tempo[0],'\n',self.val_filenames[0],self.val_tempo[0])
         print(f'Found {len(self.train_filenames)} train samples\nFound {len(self.val_filenames)} validation samples')
@@ -55,14 +66,23 @@ class Dataset:
         # Convert the waveform to a spectrogram via a STFT.
         # spectrogram = tf.signal.stft(
         #     waveform, frame_length=255, frame_step=128)
-        spectrogram = librosa.cqt(waveform.numpy())
-        # Obtain the magnitude of the CQT.
-        spectrogram = tf.abs(spectrogram)
         # Add a `channels` dimension, so that the spectrogram can be used
         # as image-like input data with convolution layers (which expect
         # shape (`batch_size`, `height`, `width`, `channels`).
-        spectrogram = spectrogram[..., tf.newaxis]
+        if self.fft:
+            # Convert waveform to spectrogram using STFT
+            spectrogram = tfio.audio.spectrogram(waveform, nfft=self.n_fft, window=512, stride=256)
+            mel_spectrogram = tfio.audio.melscale(spectrogram, rate=self.sample_rate, mels=128, fmin=0, fmax=8000)
+            dbscale_mel_spectrogram = tfio.audio.dbscale(mel_spectrogram, top_db=80)
+            spectrogram = dbscale_mel_spectrogram[..., tf.newaxis]
+        else:
+            # Convert waveform to spectrogram using CQT
+            cqt_spectrogram = librosa.cqt(waveform.numpy(), sr=self.sample_rate, n_bins=60 * 2, bins_per_octave=12 * 2)
+            # Obtain the magnitude of the spectrogram.
+            cqt_spectrogram = tf.abs(cqt_spectrogram)
+            spectrogram = cqt_spectrogram[..., tf.newaxis]
         return spectrogram, tempo
+        # return spectogram, tempo
 
     def get_spectrogram_generator(self):
         self.get_data()
@@ -85,23 +105,25 @@ class Dataset:
         width = log_spec.shape[1]
         X = np.linspace(0, np.size(spectrogram), num=width, dtype=int)
         Y = range(height)
-        ax.pcolormesh(X, Y, log_spec)
+        ax.pcolormesh(X, Y, log_spec, cmap='magma')
 
 if __name__=='__main__':
     MANIFEST_PATH = 'E:\\TempoTransformer\\data\\final_manifest_new.csv'
-    d = Dataset(MANIFEST_PATH, val_split=0.05)
+    d = TempoDataset(MANIFEST_PATH, val_split=0.05,n_fft=2048, fft=False)
     train,val = d.get_spectrogram_generator()
 
-    rows = 3
-    cols = 3
+    rows = 2
+    cols = 2
     n = rows*cols
     fig, axes = plt.subplots(rows, cols, figsize=(10, 10))
 
     for i, (spectrogram, tempo) in enumerate(train.take(n)):
+        if i==0:
+            print(spectrogram.numpy().shape)
         r = i // cols
         c = i % cols
         ax = axes[r][c]
-        Dataset.plot_spectrogram(spectrogram.numpy(), ax)
+        TempoDataset.plot_spectrogram(spectrogram.numpy(), ax)
         ax.set_title(tempo.numpy())
         ax.axis('off')
 
